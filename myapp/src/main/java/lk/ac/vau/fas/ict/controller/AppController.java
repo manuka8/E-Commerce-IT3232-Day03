f813f46402e2d3c0de8a4d812645c559ddb73beb
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-03-29 21:25:39 +0530

    Define routes and logics

---
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -0,0 +1,61 @@
+package lk.ac.vau.fas.ict.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.ac.vau.fas.ict.model.Student;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+	
+	Student Bob = new Student("2020ICT43","Bob",56,"IT",95.0);
+	Student Alice = new Student("2020ICT49","Aice",56,"AMC",140);
+	Student George = new Student("2020ICT92","Gerge",56,"Bio",56.0);
+	
+	List<Student> students= new ArrayList<Student>();
+	@GetMapping("/students")
+	public List<Student> getStudent() {
+		students.add(Alice);
+		students.add(Bob);
+		students.add(George);
+		
+		return students;
+	}
+	@GetMapping("/regno/{rg}")
+	public Student StudentDetals(@PathVariable("rg") String rg) {
+		for (Student student : students) {
+			if(student.getRegNo().equals(rg)) {
+				return student;
+			}
+		}
+		return null;
+		
+	}
+
+	@GetMapping("/msg")
+	public String myMessage() {
+		return "Hello SprintBoot";
+	}
+	
+	@GetMapping("/name")
+	public String myName() {
+		return "My name is SprintBoot";
+	}
+	@GetMapping("/reg")
+	public String myReg() {
+		return "My reg number 2020/ICT/43 /n u";
+	}
+	
+	@GetMapping("/age/{ag}")
+	public String MyAge(@PathVariable("ag") int age) {
+		return "My age is"+age;
+		
+	}
+
+}
