71ea9d7efdc5e04b450dc223c72c5ca3ff622224
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-03-29 22:38:16 +0530

    Define route for add a new student

--- myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,7 @@ public class AppController {
 
 		return students;
 	}
-
+	
 	@GetMapping("/regno/{rg}")
 	public Student StudentDetals(@PathVariable("rg") String rg) {
 		for (Student student : students) {
@@ -79,6 +80,12 @@ public class AppController {
 
 		return sortedStudents;
 	}
+	//CRUD Operations
+	@PostMapping
+    public String addStudent(@RequestBody Student student) {
+        students.add(student);
+        return "Student added successfully!";
+    }
 
 	@PutMapping("/{regNo}")
 	public String updateStudent(@PathVariable("regNo") String regNo, @RequestBody Student updatedStudent) {
@@ -101,11 +108,4 @@ public class AppController {
 		}
 		return "Student not found!";
 	}
-
-	@GetMapping("/age/{ag}")
-	public String MyAge(@PathVariable("ag") int age) {
-		return "My age is" + age;
-
-	}
-
 }
